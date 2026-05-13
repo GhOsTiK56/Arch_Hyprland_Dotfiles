9ae7cfb3fd0e544ff2607dd831c5eaba02c18949
Ghostik <karenheister5@gmail.com>
2026-05-13 16:48:00 +0300

    feat: add old config settings

--- dotfiles/hypr/.config/hypr/modules/autostart.lua
+++ dotfiles/hypr/.config/hypr/modules/autostart.lua
@@ -1,13 +1,38 @@
 -------------------
 ---- AUTOSTART ----
 -------------------
-
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
 hl.on("hyprland.start", function () 
+-- Bar / UI
   hl.exec_cmd("waybar")
+
+-- Cursor
+  hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 28")
+
+-- Keyring
+  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+
+-- Polkit agent
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+
+-- Clipboard tools
+  hl.exec_cmd("wl-clip-persist --clipboard regular")
+  hl.exec_cmd("wl-paste --type text --watch cliphist store")
+  hl.exec_cmd("wl-paste --type image --watch cliphist store")
+
+-- DBus environment sync
+  hl.exec_cmd("dbus-update-activation-environment --systemd --all")
+
+ -- Delayed secret check
+  hl.exec_cmd("sh -c 'sleep 3 && secret-tool lookup dummy test >/dev/null 2>&1 || true'")
+
+  -- Wallpaper daemon
+  hl.exec_cmd("swww-daemon")
+
+  -- Notifications
   hl.exec_cmd("swaync")
+
+  -- RGB control with delay
+  hl.exec_cmd("sh -c 'sleep 4 && openrgb --startminimized --profile Black'")
 end)
