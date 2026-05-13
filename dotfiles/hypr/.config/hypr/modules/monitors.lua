01532a36663a8f6c2a5bbde05eed2e9add84e0f8
Ghostik <karenheister5@gmail.com>
2026-05-13 20:31:06 +0300

    feat: delete nvChad and install lazyVim

--- dotfiles/hypr/.config/hypr/modules/monitors.lua
+++ dotfiles/hypr/.config/hypr/modules/monitors.lua
@@ -5,7 +5,7 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "HDMI-A-2",
-    mode     = "1920x1080@74.99",
+    mode     = "1920x1080@74.90",
     position = "auto",
     scale    = "auto",
 })
