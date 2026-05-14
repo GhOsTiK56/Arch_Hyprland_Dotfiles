7d74d81766775e084cb5a627877237c10d050e0a
Ghostik <karenheister5@gmail.com>
2026-05-14 14:00:20 +0300

    add PATH variables

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,34 +1,35 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-	fastfetch
-  alias ls='lsd'
-  alias l='ls -l'
-  alias la='ls -a'
-  alias lla='ls -la'
-  alias lt='ls --tree'
-  alias cat='bat'
-  zoxide init fish | source
+    # Commands to run in interactive sessions can go here
+    fastfetch
+    alias ls='lsd'
+    alias l='ls -l'
+    alias la='ls -a'
+    alias lla='ls -la'
+    alias lt='ls --tree'
+    alias cat='bat'
+    zoxide init fish | source
 end
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
-
 
 set fish_greeting
 
-# Flutter
+# Android & Flutter
 set -gx PATH /home/ghostik/Documents/Android/flutter/bin $PATH
 set -gx PATH /home/ghostik/Documents/Android/Sdk $PATH
+set -gx PATH /home/ghostik/Documents/Android/Sdk/cmdline-tools/latest/bin/ $PATH
+set -gx PATH /home/ghostik/Documents/Android/Sdk/platform-tools/ $PATH
+set -gx PATH /home/ghostik/Documents/Android/Sdk/emulator/ $PATH
 
 # npm-global
 set -gx PATH ~/.npm-global/bin $PATH
-
